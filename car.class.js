54b9267abd728f467a65ccd371f2e725c9c734ef
Cédric PAIMBLANC <19099685@GRN164.local>
2021-07-01 13:29:54 +0200

    attribut Creation

--- car.class.js
+++ car.class.js
@@ -1,1 +1,39 @@
-function Voiture()
+function Voiture(newImmatriculation,newColor,newWeight,newPower,newFuel,newNbPlace){
+    var immatriculation;
+    var color
+    var weight;
+    var power;
+    var fuelCapacity ;
+    var nbPlace;
+    var insure;
+    var boardMsg;
+
+    
+
+    //Getters and Setters
+
+
+
+    var setImmatriculation=function(){};
+    var setWeight=function(){};
+    var setPower=function(){};
+    var setFuelCapacity=function(){};
+    var setNbPlace=function(){};
+    this.setInsure=function(){};
+    this.setBordMsg=function(){};
+
+    this.getImmatriculation=function(){};
+    this.getWeight=function(){};
+    this.getPower=function(){};
+    this.getFuelCapacity=function(){};
+    this.getNbPlace=function(){};
+    this.getInsure=function(){};
+    this.getBordMsg=function(){};
+    // Initialisation
+
+    // Action
+
+
+    //toString
+     
+}
