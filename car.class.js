3e97c6a117e3b50b5ad4d3e1a8e3fa053820a2c6
Louis <69362039+U-Louis@users.noreply.github.com>
2021-07-01 14:02:52 +0200

    Update car.class.js

--- car.class.js
+++ car.class.js
@@ -1,37 +1,3 @@
-function Voiture(newImmatriculation,newColor,newWeight,newPower,newFuel,newNbPlace){
-    var immatriculation;
-    var color
-    var weight;
-    var power;
-    var fuelCapacity ;
-    var nbPlace;
-    var insure;
-    var boardMsg;
-    
-
-    //Getters and Setters
-    var setImmatriculation=function(){};
-    var setWeight=function(){};
-    var setPower=function(){};
-    var setFuelCapacity=function(){};
-    var setNbPlace=function(){};
-    this.setInsure=function(){};
-    this.setBordMsg=function(){};
-
-    this.getImmatriculation=function(){};
-    this.getWeight=function(){};
-    this.getPower=function(){};
-    this.getFuelCapacity=function(){};
-    this.getNbPlace=function(){};
-    this.getInsure=function(){};
-    this.getBordMsg=function(){};
-    // Initialisation
-
-    // Action
-
-
-    //toString
-     
 //Methods
 function Voiture(newImmatriculation, newColor, newWeight, newPower, newFuel, newNbPlace) {
     var immatriculation;
